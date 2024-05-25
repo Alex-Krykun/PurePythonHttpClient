52349d5cb9f9e189197e6d80fb0ccd87f6cecec4
Alex Krykun <54208127+Alex-Krykun@users.noreply.github.com>
2024-05-24 23:48:46 -0700

    add support of gzip for echo

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from pathlib import Path
 import socket
 import threading
+import gzip
 
 HOST = os.getenv("HOST", "localhost")
 PORT = os.getenv("PORT", 4221)
@@ -14,6 +15,10 @@ HTTP_REASON_PHRASE = {
     201: "Created",
     404: "Not Found"
 }
+
+SUPPORTED_ENCODING = [
+    'gzip'
+]
 
 
 def create_socket_server():
@@ -52,13 +57,24 @@ def build_response(status_code, headers=None, body=None) -> str:
         if not headers.get('Content-Type'):
             headers['Content-Type'] = 'text/plain'
 
-        headers['Content-Length'] = str(len(body))
+        if not headers.get('Content-Length'):
+            headers['Content-Length'] = str(len(body))
 
     headers_str = ''
     for key, value in headers.items():
-        headers_str += f'{key}: {value}{CRLF}'
+        headers_str += f'{key.lower()}: {value}{CRLF}'
 
     return f"HTTP/1.1 {status_code} {HTTP_REASON_PHRASE[status_code]}{CRLF}{headers_str}{CRLF}{body or ''}"
+
+
+def get_content_encoding(accept_encoding: str):
+    accept_encoding = accept_encoding.split(", ")
+    response_content_encoding = []
+    for req_encoding in accept_encoding:
+        if req_encoding in SUPPORTED_ENCODING:
+            response_content_encoding.append(req_encoding)
+
+    return ", ".join(response_content_encoding) if response_content_encoding else None
 
 
 def routes(path: str, method='GET', headers=None, body=None) -> str:
@@ -68,7 +84,17 @@ def routes(path: str, method='GET', headers=None, body=None) -> str:
 
     if segments[0] == 'echo':
         body = segments[1]
-        return build_response(200, body=body)
+        response_header = None
+        if headers.get('Accept-Encoding'):
+            content_encoding = get_content_encoding(headers['Accept-Encoding'])
+            if content_encoding:
+                response_header = {
+                    "Content-Encoding": content_encoding,
+                    "Content-Length": str(len(body)),
+                }
+
+                body = gzip.compress(body.encode('utf-8'))
+        return build_response(200, headers=response_header, body=body)
 
     if segments[0] == 'user-agent':
         body = headers['User-Agent']
@@ -101,7 +127,7 @@ def routes(path: str, method='GET', headers=None, body=None) -> str:
 
 def handle_request(user_socket):
     request = parse_request(user_socket.recv(1024).decode())
-    response = routes(request['path'], request['method'], headers=request['headers'], body=request['body'])
+    response = routes(request['path'], request['http_method'], headers=request['headers'], body=request['body'])
     user_socket.send(response.encode())
 
 
